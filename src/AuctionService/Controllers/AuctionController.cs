1fc02a89d40e94a04c243c4478a0a153d440bd47
drazen <drazen.cvjetkovic777@gmail.com>
2024-05-02 18:51:50 +0200

    Auction created

--- src/AuctionService/Controllers/AuctionController.cs
+++ src/AuctionService/Controllers/AuctionController.cs
@@ -3,103 +3,107 @@ using AuctionService.DTOs;
 using AuctionService.Entities;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
+using Contracts;
+using MassTransit;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace AuctionService.Controllers
+namespace AuctionService.Controllers;
+
+[ApiController]
+[Route("api/auctions")]
+public class AuctionController : ControllerBase
 {
-    [ApiController]
-    [Route("api/auctions")]
-    public class AuctionController : ControllerBase
+    private readonly AuctionDbContext _context;
+    private readonly IMapper _mapper;
+    private readonly IPublishEndpoint _publishEndpoint;
+
+    public AuctionController(AuctionDbContext context, IMapper mapper, IPublishEndpoint publishEndpoint)
     {
-        private AuctionDbContext _context;
-        private readonly IMapper _mapper;
+        _context = context;
+        _mapper = mapper;
+        _publishEndpoint = publishEndpoint;
+    }
 
-        public AuctionController(AuctionDbContext context, IMapper mapper)
-        {
-            _context = context;
-            _mapper = mapper;
-        }
+    [HttpGet]
+    public async Task<ActionResult<List<AuctionDto>>> GetAllAuctions(string date)
+    {
+        var query = _context.Auctions.OrderBy(x => x.Item.Make).AsQueryable();
+        if (!string.IsNullOrEmpty(date))
+            query = query.Where(x => x.UpdatedAt.CompareTo(DateTime.Parse(date).ToUniversalTime()) > 0);
 
-        [HttpGet]
-        public async Task<ActionResult<List<AuctionDto>>> GetAllAuctions(string date)
-        {
+        return await query.ProjectTo<AuctionDto>(_mapper.ConfigurationProvider).ToListAsync();
 
-            var query = _context.Auctions.OrderBy(x => x.Item.Make).AsQueryable();
-            if (!string.IsNullOrEmpty(date))
-            {
-                query = query.Where(x => x.UpdatedAt.CompareTo(DateTime.Parse(date).ToUniversalTime()) > 0);
-            }
+        // var auctions = await _context.Auctions
+        //     .Include(x => x.Item)
+        //     .OrderBy(x => x.Item.Make)
+        //     .ToListAsync();
+        //
+        // return _mapper.Map<List<AuctionDto>>(auctions);
+    }
 
-            return await query.ProjectTo<AuctionDto>(_mapper.ConfigurationProvider).ToListAsync();
+    [HttpGet("{Id}")]
+    public async Task<ActionResult<AuctionDto>> GetAuctionById(Guid id)
+    {
+        var auction = await _context.Auctions.Include(x => x.Item)
+            .FirstOrDefaultAsync(x => x.Id == id);
 
-            // var auctions = await _context.Auctions
-            //     .Include(x => x.Item)
-            //     .OrderBy(x => x.Item.Make)
-            //     .ToListAsync();
-            //
-            // return _mapper.Map<List<AuctionDto>>(auctions);
-        }
+        if (auction == null) return NotFound();
 
-        [HttpGet("{Id}")]
-        public async Task<ActionResult<AuctionDto>> GetAuctionById(Guid id)
-        {
-            var auction = await _context.Auctions.Include(x => x.Item)
-                .FirstOrDefaultAsync(x => x.Id == id);
+        return _mapper.Map<AuctionDto>(auction);
+    }
 
-            if (auction == null) return NotFound();
+    [HttpPost]
+    public async Task<ActionResult<AuctionDto>> CreateAuction(CreateAuctionDto auctionDto)
+    {
+        var auction = _mapper.Map<Auction>(auctionDto);
+        auction.Seller = "test";
+        _context.Auctions.Add(auction);
 
-            return _mapper.Map<AuctionDto>(auction);
-        }
+        var result = await _context.SaveChangesAsync() > 0;
 
-        [HttpPost]
-        public async Task<ActionResult<AuctionDto>> CreateAuction(CreateAuctionDto auctionDto)
-        {
-            var auction = _mapper.Map<Auction>(auctionDto);
-            auction.Seller = "test";
-            _context.Auctions.Add(auction);
+        var newAuction = _mapper.Map<AuctionDto>(auction);
+        await _publishEndpoint.Publish(_mapper.Map<AuctionCreated>(newAuction));
 
-            var result = await _context.SaveChangesAsync() > 0;
+        if (!result) return BadRequest("Could not save changes to the DB");
 
-            if (!result) return BadRequest("Could not save changes to the DB");
+        return CreatedAtAction(nameof(GetAuctionById), new { auction.Id },
+            newAuction);
+    }
 
-            return CreatedAtAction(nameof(GetAuctionById), new { auction.Id }, _mapper.Map<AuctionDto>(auction));
+    [HttpPut("{id}")]
+    public async Task<ActionResult> UpdateAuction(Guid id, UpdateAuctionDto updateAuctionDto)
+    {
+        var auction = await _context.Auctions.Include(x => x.Item).FirstOrDefaultAsync(x => x.Id == id);
 
-        }
+        if (auction == null) return NotFound();
 
-        [HttpPut("{id}")]
-        public async Task<ActionResult> UpdateAuction(Guid id, UpdateAuctionDto updateAuctionDto)
-        {
-            var auction = await _context.Auctions.Include(x => x.Item).FirstOrDefaultAsync(x => x.Id == id);
+        auction.Item.Make = updateAuctionDto.Make ?? auction.Item.Make;
+        auction.Item.Model = updateAuctionDto.Model ?? auction.Item.Model;
+        auction.Item.Color = updateAuctionDto.Color ?? auction.Item.Color;
+        auction.Item.Mileage = updateAuctionDto.Mileage ?? auction.Item.Mileage;
+        auction.Item.Year = updateAuctionDto.Year ?? auction.Item.Year;
 
-            if (auction == null) return NotFound();
+        var result = await _context.SaveChangesAsync() > 0;
 
-            auction.Item.Make = updateAuctionDto.Make ?? auction.Item.Make;
-            auction.Item.Model = updateAuctionDto.Model ?? auction.Item.Model;
-            auction.Item.Color = updateAuctionDto.Color ?? auction.Item.Color;
-            auction.Item.Mileage = updateAuctionDto.Mileage ?? auction.Item.Mileage;
-            auction.Item.Year = updateAuctionDto.Year ?? auction.Item.Year;
+        if (result) return Ok();
 
-            var result = await _context.SaveChangesAsync() > 0;
+        return BadRequest("Problem saving changes!");
+    }
 
-            if (result) return Ok();
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> DeleteAuction(Guid id)
+    {
+        var auction = await _context.Auctions.FindAsync(id);
 
-            return BadRequest("Problem saving changes!");
-        }
+        if (auction == null) return NotFound();
 
-        [HttpDelete("{id}")]
-        public async Task<ActionResult> DeleteAuction(Guid id)
-        {
-            var auction = await _context.Auctions.FindAsync(id);
+        _context.Auctions.Remove(auction);
 
-            if (auction == null) return NotFound();
+        var result = await _context.SaveChangesAsync() > 0;
 
-            _context.Auctions.Remove(auction);
+        if (!result) return BadRequest("Could not delete from DB");
 
-            var result = await _context.SaveChangesAsync() > 0;
-
-            if (!result) return BadRequest("Could not delete from DB");
-
-            return Ok();        }
+        return Ok();
     }
 }
--- src/AuctionService/RequestHelpers/MappingProfiles.cs
+++ src/AuctionService/RequestHelpers/MappingProfiles.cs
@@ -1,17 +1,19 @@
 ﻿using AuctionService.DTOs;
 using AuctionService.Entities;
 using AutoMapper;
-namespace AuctionService.RequestHelpers
+using Contracts;
+
+namespace AuctionService.RequestHelpers;
+
+public class MappingProfiles : Profile
 {
-    public class MappingProfiles : Profile
+    public MappingProfiles()
     {
-        public MappingProfiles()
-        {
-            CreateMap<Auction, AuctionDto>().IncludeMembers(x => x.Item);
-            CreateMap<Item, AuctionDto>();
-            CreateMap<CreateAuctionDto, Auction>()
-                .ForMember(d => d.Item, o => o.MapFrom(s => s));
-            CreateMap<CreateAuctionDto, Item>();
-        }
+        CreateMap<Auction, AuctionDto>().IncludeMembers(x => x.Item);
+        CreateMap<Item, AuctionDto>();
+        CreateMap<CreateAuctionDto, Auction>()
+            .ForMember(d => d.Item, o => o.MapFrom(s => s));
+        CreateMap<CreateAuctionDto, Item>();
+        CreateMap<AuctionDto, AuctionCreated>();
     }
 }
